a9a53a667047e2ea0ec6c9aa1eb5c44940a76824
warmab13 <alonsosalcido13@gmail.com>
2022-11-01 12:23:46 -0700

    add babel configuration to use vue js on public folder of project

--- src/public/app.js
+++
@@ -1,1 +0,0 @@
-alert('Vuejs goes here')
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const { VueLoaderPlugin } = require('vue-loader');
+module.exports = {
+    entry: './src/app/index.js',
+    output: {
+        path: __dirname + '/src/public/js',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /nodemodules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
