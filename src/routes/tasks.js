3fbb8b05cbb81e01e00d7fb477fd7281cf2ed346
warmab13 <alonsosalcido13@gmail.com>
2022-11-01 10:46:19 -0700

    use promises to return data on asyncrhonous callback

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const Task = require('../models/Task');
 
 router.get('/', (req, res)=>{
-    Task.find(function(data){
-        res.json(data);
-    })
+    Task.find() //Using promises
+        .then(data=>{
+            res.json(data)
+        })
 })
 
 module.exports = router;
