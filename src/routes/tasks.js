221551c2c2f84b7c693b27cfeaab406a4adad140
warmab13 <alonsosalcido13@gmail.com>
2022-11-01 10:59:16 -0700

    use async await to return data from database

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const Task = require('../models/Task');
 
-router.get('/', (req, res)=>{
-    Task.find() //Using promises
-        .then(data=>{
-            res.json(data)
-        })
+router.get('/', async (req, res)=>{
+    const tasks = await Task.find();
+    console.log("🚀 ~ file: tasks.js ~ line 8 ~ router.get ~ tasks", tasks)
+    res.json(tasks);
+})
+
+router.post('/', async (req, res)=>{
+    
 })
 
 module.exports = router;
