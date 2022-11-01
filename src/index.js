4e2859392959fdf8d7732227abf6adceff3f7655
warmab13 <alonsosalcido13@gmail.com>
2022-11-01 09:56:12 -0700

    process env variables and setup static files on server

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 
 //Settings
+app.set( 'port', process.env.PORT || 3000 );
 
 //Middlewares
 
 //Routes
 
+//Static files
+app.use(express.static(__dirname + '/public'))
+
 //Server listening
-app.listen(3000, ()=>{
-    console.log('Server on port ' + 3000)
-})
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port ', app.get('port'));
+});
